c89f815982b756539cbf7990183f14636476e84f
Carlos Javier <cjangie13@gmail.com>
2020-10-23 18:00:58 -0400

    introduciendo modulo os para acceso al bajo nivel

---
+++ modulos/os.js
@@ -0,0 +1,39 @@
+//clase 18
+const { maxHeaderSize } = require('http');
+const os = require('os');
+
+//console.log(os.arch());
+//console.log(os.platform());
+
+//console.log(os.cpus())
+//console.log(os.cpus().length)
+
+//console.log(os.constants);
+
+
+const SIZE =1024
+const kb=(bytes)=>{
+   return bytes/SIZE;
+}
+const mb=(bytes)=>{
+   return kb(bytes/SIZE);
+}
+const gb=(bytes)=>{
+   return mb(bytes/SIZE);
+}
+
+
+
+console.log(os.freemem());
+console.log(kb(os.freemem()));
+console.log(mb(os.freemem()));
+console.log(gb(os.freemem()));
+console.log(gb(os.totalmem()));
+
+// esto es memoria ram
+
+// console.log(os.homedir());
+// console.log(os.tmpdir());
+
+// console.log(os.hostname());
+console.log(os.networkInterfaces());
