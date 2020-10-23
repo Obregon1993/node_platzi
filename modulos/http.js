a3bea775aae5bfdb5248bbcea9030c9c435c87a6
Carlos Javier <cjangie13@gmail.com>
2020-10-23 17:44:12 -0400

    agregando crear server con hhtp

---
+++ modulos/http.js
@@ -0,0 +1,54 @@
+const http = require('http');
+
+//asi seria normal el otro ejemplo es para hacerlo mas funcional utilizando la funcion router
+
+// http.createServer((req, res) => {
+//     console.log('nueva peticion');
+//     console.log(req.url);
+//     //escribir respuesta al usuario
+
+//     res.writeHead(201, {'Content-Type': 'text/plain'})
+
+//     res.write('hola, ya se escribir usando http');
+//     res.end();
+// }).listen(3000);
+
+// console.log('escuchando en el puerto 3000')
+//---------------------------------------------------------------------
+http.createServer(router).listen(3000);
+//este tipo de funcion no es legible para http, deberia ser sin es6
+// const router=(req, res) => {
+//     console.log('nueva peticion');
+//     console.log(req.url);
+//     //escribir respuesta al usuario
+
+//     res.writeHead(201, {'Content-Type': 'text/plain'})
+
+//     res.write('hola, ya se escribir usando http');
+//     res.end();
+// }
+//--------------------------------------------
+function router(req, res) {
+    console.log('nueva peticion');
+    console.log(req.url);
+    //escribir respuesta al usuario
+    switch (req.url) {
+        case '/hola':
+            res.write('Hola, que tal')
+            res.end();
+            break;
+
+        default:
+            res.write('Error 404: nose lo que kieres');
+            res.end();
+    }
+
+    // res.writeHead(201, { 'Content-Type': 'text/plain' })
+
+    // res.write('hola, ya se escribir usando http');
+    // res.end();
+}
+
+
+console.log('escuchando en el puerto 3000');
+
